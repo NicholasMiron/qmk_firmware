ce3c132d53de98fe0146a8d88856d813603ee3f1
Nick <nicholasmiron@gmail.com>
2025-06-19 09:37:46 -0600

    Adding in equals sign

--- keyboards/planck/keymaps/NicholasMiron/keymap.c
+++ keyboards/planck/keymaps/NicholasMiron/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_LPRN,    KC_RPRN,    KC_MINS, KC_PLUS, _______,
     _______, KC_GRV,  KC_TILD, KC_QUOT, KC_DQUO, KC_AMPR, KC_PIPE, KC_LCBR,    KC_RCBR,    KC_SLSH, KC_ASTR, _______,
-    _______, _______, KC_UNDS, KC_LT,   KC_GT,   _______, _______, KC_LBRC,    KC_RBRC,    _______, _______, _______,
+    _______, _______, KC_UNDS, KC_LT,   KC_GT,   _______, _______, KC_LBRC,    KC_RBRC,    _______, KC_EQL,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
 ),
 
